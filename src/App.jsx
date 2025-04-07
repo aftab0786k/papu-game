9cf3bf43b688c3a6d31d630881afacf75beca02f
aftab0786k <148603389+aftab0786k@users.noreply.github.com>
2025-04-07 16:18:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Papu from './Components/Papu';
-import Home from './Home';
+// import Home from './Home';
 import Card from './Components/Card';
 
 const App = () => {
   return (
     <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="papu" element={<Papu />} />
+{/*     <Route path="/" element={<Home />} /> */}
+    <Route path="/" element={<Papu />} />
     <Route path="card" element={<Card />} />
     
   </Routes>
